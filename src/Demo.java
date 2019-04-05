c338dad9d8eabbe7e92a52c8e5ed8b26638be808
huangguanzhong <2534382535@qq.com>
2019-04-05 17:38:51 +0800

    ssss

---
+++ src/Demo.java
@@ -0,0 +1,18 @@
+public class Demo {
+    public static void main(String[] args) {
+
+       String s="qwe,wqeq";
+        String s2 = s.substring(1, s.length() - 1);
+        System.out.println(s2);
+//        int sum=100;
+//        int num=0;
+//        for (int i = 1; i <=10 ; i++) {
+//            sum=sum/2;
+//            num+=sum;
+//            System.out.println(sum+" ");
+//        }
+//        System.out.println(num+"");
+   }
+
+
+}
